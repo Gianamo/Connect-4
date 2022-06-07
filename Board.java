f60de337aafcea22b740b309931ac4e26212109e
Evan <ejyavor@gmail.com>
2022-06-07 10:09:33 -0400

    First commit of connect 4 board

--- Board.java
+++ Board.java
@@ -2,32 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 public class Board extends JFrame
 {
-    public static Square[][] board = new Square[8][8];
-
+    public static Square[][] board = new Square[6][7];
+    int lastrow;
+    int lastcol;
     public Board()
     {
-        setLayout(new GridLayout(8,8));
-        for(int r = 0; r < 8; r++)
+        setLayout(new GridLayout(6,7));
+        for(int r = 0; r < 6; r++)
         {
-            for(int c = 0; c < 8; c++)
+            for(int c = 0; c < 7; c++)
             {
-                if(r < 3)
-                {
-                    board[r][c]= new Square(1, 1, r, c);
-                }
-                else if(r < 5)
-                {
-                    board[r][c]= new Square(0, 0, r, c);
-                }
-                else
-                {
-                    board[r][c]= new Square(1, 2, r, c);
-                }
-                if(!(board[r][c].getValid()))
-                {
-                    board[r][c].setRank(0);
-                    board[r][c].setColor(0);
-                }
+                board[r][c]= new Square(r, c);
             }
         }
 
@@ -38,160 +23,160 @@ public class Board extends JFrame
         }
     }
 
-    private boolean isMoveValid(int turn, int sx, int sy, int ex, int ey)
+    private boolean isMoveValid(int col)
     {
-        boolean output = ((0 <= sx && sx <= 7)&&(0 <= sy && sy <= 7) && (0 <= ex && ex <= 7)&&(0 <= ey && ey <= 7));
-        if(output && turn != board[sx][sy].getColor())
-        {
-            output = false;
-        }
-        if(output && turn == board[ex][ey].getColor())
-        {
-            output = false;
-        }
-        if(output &&(!board[ex][ey].getValid() || !board[sx][sy].getValid()))
-        {
-            output = false;
-        }
-        if(!(sx-2 <= ex && ex <= sx+2) || !(sy-2 <= ey && ey <= sy+2))
-        {
-            output = false;
-        }
-        if( output &&((sx == ex)||(sy==ey)))
-        {
-            output = false;
-        }
-        if(output && board[sx][sy].getRank()== 1 && sx>ex && turn == 1)
-        {
-            output = false;
-        }
-        if(output && board[sx][sy].getRank()== 1 && sx<ex && turn == 2)
-        {
-            output = false;
-        }
-        if(output)
-        {
-            if(!(sx-1 <= ex && ex <= sx+1) || !(sy-1 <= ey && ey <= sy+1))
-            {
-                if(board[(ex + sx)/2][(sy + ey)/2].getColor() == turn || board[(ex + sx)/2][(sy + ey)/2].getColor() == 0)
-                {
-                    output = false;
-                }
-            }
-        }
-        return output;
+        return board[0][col].getColor() == 0;
     }
 
-    public void print()
+    public boolean dropPiece(int turn, int col)
     {
-        for(int i = 0; i<10; i++)
+        int temp = 3;
+        int i = 7;
+        if(isMoveValid(col))
         {
-            System.out.println();
-        }
-        System.out.println("  0 1 2 3 4 5 6 7 ");
-        System.out.println(" _________________");
-        for(int r = 0; r < 8; r++)
-        {
-            System.out.print(r + "|");
-            for(int c = 0; c < 8; c++)
+            while(temp != 0)
             {
-                if(board[r][c].getColor() == 0)
-                {
-                    System.out.print(" ");
-                }
-                else if(board[r][c].getColor() == 1)
-                {
-                    if(board[r][c].getRank() == 1)
-                    {
-                        System.out.print("r");
-                    }
-                    else
-                    {
-                        System.out.print("R");
-                    }
-                }
-                else
-                {
-                    if(board[r][c].getRank() == 1)
-                    {
-                        System.out.print("b");
-                    }
-                    else
-                    {
-                        System.out.print("B");
-                    }
-                }
-                System.out.print("|");
+                i--;
+                temp = board[i][col].getColor();
             }
-            System.out.println(r);
-            System.out.println(" _________________");
+            board[i][col].setColor(turn);
+            gameWinner(turn,i,col);
+            return true;
         }
-        System.out.println("  0 1 2 3 4 5 6 7 ");
+        return false;
     }
-
-    public boolean move(int turn, int sx, int sy, int ex, int ey)
+    
+    public void gameWinner(int turn, int row, int  col)
     {
-        boolean bruh = isMoveValid(turn, sx, sy, ex, ey);
-        if(isMoveValid(turn, sx, sy, ex, ey))
+        int hor = 1;
+        int vert = 1;
+        int slash = 1;
+        int backslash = 1;
+        boolean valid;
+        int i;
+        
+        //hor checker
+        i = 1; 
+        valid = true;
+        while(valid && col + i <= 6 )
         {
-            board[ex][ey].setColor(board[sx][sy].getColor());
-            board[ex][ey].setRank(board[sx][sy].getRank());
-            board[sx][sy].reset();
-            if(isAttack(sx, sy, ex, ey))
+            if(board[row][col + i].getColor() == turn)
             {
-                board[(sx+ex)/2][(sy+ey)/2].reset();
-                board[(sx+ex)/2][(sy+ey)/2].getPiece().updatePiece();
+                hor++;
+                i++;
             }
-            if((turn == 1 && ex == 7)||(turn == 2 && ex == 0))
+            else
             {
-                makeKing(ex, ey);
-            }
-
-            board[ex][ey].getPiece().updatePiece();
-            board[sx][sy].getPiece().updatePiece();
-        }
-        return bruh;
-    }
-
-    private boolean isAttack(int sx, int sy, int ex, int ey)
-    {
-        return ((sx-1 > ex || ex > sx+1) || (sy-1 > ey || ey > sy+1));
-    }
-
-    public void makeKing(int x, int y)
-    {
-        board[x][y].setRank(2);
-    }
-
-    public int gameWinner()
-    {
-        boolean isWDed = true;
-        boolean isBDed = true;
-        for(int x = 0; x < 7; x++)
-        {
-            for(int y = 0; y < 7; y++)
-            {
-                if (board[x][y].getColor()== 1)
-                {
-                    isWDed = false;
-                }
-                if (board[x][y].getColor()== 2)
-                {
-                    isBDed = false;
-                }
+                valid = false;
             }
         }
-        if(isBDed)
+        i = -1;
+        valid = true;
+        while(valid && col + i >= 0)
         {
-            return 1;
+            if(board[row][col + i].getColor() == turn)
+            {
+                hor++;
+                i--;
+            }
+            else
+            {
+                valid = false;
+            }
         }
-        else if(isWDed)
+        
+        //vert checker
+        i = 1; 
+        valid = true;
+        while(valid && row + i <= 5 )
         {
-            return 2;
+            if(board[row + i][col].getColor() == turn)
+            {
+                vert++;
+                i++;
+            }
+            else
+            {
+                valid = false;
+            }
         }
-        else
+        i = -1;
+        valid = true;
+        while(valid && row + i >= 0)
         {
-            return 0;
+            if(board[row + i][col].getColor() == turn)
+            {
+                vert++;
+                i--;
+            }
+            else
+            {
+                valid = false;
+            }
+        }
+        
+        //slash checker (/)
+        i = 1; 
+        valid = true;
+        while(valid &&(row + i <= 5 && col - i >= 0))
+        {
+            if(board[row + i][col - i].getColor() == turn)
+            {
+                slash++;
+                i++;
+            }
+            else
+            {
+                valid = false;
+            }
+        }
+        i = -1;
+        valid = true;
+        while(valid &&(row + i >= 0 && col - i <= 6))
+        {
+            if(board[row + i][col - i].getColor() == turn)
+            {
+                slash++;
+                i--;
+            }
+            else
+            {
+                valid = false;
+            }
+        }
+        
+        //backslash checker (\)
+        i = 1; 
+        valid = true;
+        while(valid &&(row + i <= 5 && col + i <= 6))
+        {
+            if(board[row + i][col + i].getColor() == turn)
+            {
+                hor++;
+                i++;
+            }
+            else
+            {
+                valid = false;
+            }
+        }
+        i = -1;
+        valid = true;
+        while(valid &&(row + i >= 0 && col + i >= 0))
+        {
+            if(board[row + i][col + i].getColor() == turn)
+            {
+                hor++;
+                i--;
+            }
+            else
+            {
+                valid = false;
+            }
+        }
+        if((backslash >=4)||(slash >=4)||(vert >=4)||(hor >= 4))
+        {
+            Main.gameWon = turn;
         }
     }
 }
